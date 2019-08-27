0ea641912acc282356e58fb124e73f966c154ab9
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-08-27 14:24:24 +0200

    Renamed a constant and added function to create a tetrimino easily.

--- src/UTTetrimino.pas
+++ src/UTTetrimino.pas
@@ -19,10 +19,13 @@ interface
 	function getCenterMino (t: TTetrimino) : TMino;
 	procedure setCenterMino (var t: TTetrimino; mino : Tmino);
 	
+	// Function to create tetriminos
+	function newTetrimino (shape : TShapeTetrimino) : TTetrimino;
+	
 	
 implementation
 	
-	const CENTER_MINO = 3;
+	const Ccenter_mino = 3;
 	
 	// Accessors
 	function getIthMino (t: TTetrimino; i : byte) : TMino;
@@ -48,12 +51,73 @@ implementation
 	// Useful
 	function getCenterMino (t: TTetrimino) : TMino;
 	begin
-		getCenterMino := getIthMino(t, CENTER_MINO);
+		getCenterMino := getIthMino(t, Ccenter_mino);
 	end;
 	
 	procedure setCenterMino (var t: TTetrimino; mino : Tmino);
 	begin
-		setIthMino(t, CENTER_MINO, mino);
+		setIthMino(t, Ccenter_mino, mino);
 	end;
+	
+	function newTetrimino (shape : TShapeTetrimino) : TTetrimino;
+	var
+		tetri : TTetrimino;
+	begin		
+		case shape of  //I, T, L, J, S, Z, O
+		I: begin // Initial position of the I
+			tetri.minos[1] := newMino(4,20, False, shape); // Each mino gets an initilized
+			tetri.minos[2] := newMino(5,20, False, shape);
+			tetri.minos[3] := newMino(6,20, False, shape);
+			tetri.minos[4] := newMino(7,20, False, shape);
+		   end;
+		   
+		T: begin
+			tetri.minos[1] := newMino(5,20, False, shape);
+			tetri.minos[2] := newMino(6,19, False, shape);
+			tetri.minos[3] := newMino(6,20, False, shape);
+			tetri.minos[4] := newMino(7,20, False, shape);
+		   end;
+		   
+		L: begin
+			tetri.minos[1] := newMino(5,19, False, shape);
+			tetri.minos[2] := newMino(5,20, False, shape);
+			tetri.minos[3] := newMino(6,20, False, shape);
+			tetri.minos[4] := newMino(7,20, False, shape);
+		   end;
+		   
+		J: begin
+			tetri.minos[1] := newMino(7,19, False, shape);
+			tetri.minos[2] := newMino(5,20, False, shape);
+			tetri.minos[3] := newMino(6,20, False, shape);
+			tetri.minos[4] := newMino(7,20, False, shape);
+		   end;
+		   
+		S: begin
+			tetri.minos[1] := newMino(5,19, False, shape);
+			tetri.minos[2] := newMino(6,19, False, shape);
+			tetri.minos[3] := newMino(6,20, False, shape);
+			tetri.minos[4] := newMino(7,20, False, shape);
+		   end;
+		   
+		Z: begin
+			tetri.minos[1] := newMino(5,20, False, shape);
+			tetri.minos[2] := newMino(6,19, False, shape);
+			tetri.minos[3] := newMino(6,20, False, shape);
+			tetri.minos[4] := newMino(7,19, False, shape);
+		   end;
+		   
+		O: begin
+			tetri.minos[1] := newMino(5,19, False, shape);
+			tetri.minos[2] := newMino(6,19, False, shape);
+			tetri.minos[3] := newMino(5,20, False, shape);
+			tetri.minos[4] := newMino(6,20, False, shape);
+		   end;
+		end; // End case ... of
+		
+		tetri.shape := shape;
+		newTetrimino := tetri;
+	end;
+	
+	
 	
 end.
