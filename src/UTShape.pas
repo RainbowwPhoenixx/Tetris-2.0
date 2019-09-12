c0a9d71c69e595883b7ff77c5f0f927f56f26fd7
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-09-12 18:33:38 +0200

    Fixed typo in the first line of the file.

--- src/UTShape.pas
+++ src/UTShape.pas
@@ -1,15 +1,15 @@
-unit UTshape;
+unit UTShape;
 
 interface
 	uses UConstants;
-	
+
 	Type TShapeTetrimino = (I, T, L, J, S, Z, O, VOID);
-	
+
 	function getRandomShape () : TShapeTetrimino;
 implementation
-	
+
 	function getRandomShape () : TShapeTetrimino;
 	begin
-		getRandomShape := TShapeTetrimino(random(7)); 
+		getRandomShape := TShapeTetrimino(random(7));
 	end;
 end.
