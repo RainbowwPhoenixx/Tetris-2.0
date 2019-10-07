3cc494a4499213d349ee4ec13f55bfe4a143c9fc
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-10-07 14:53:15 +0200

    Created first version of the main program. Modified compliling script accordingly. Implemented a basic execute script.

--- compile.sh
+++ compile.sh
@@ -3,6 +3,8 @@ type_unit_files=("src/UTShape.pas src/UTMino.pas src/UTTetrimino.pas src/UTMovem
 
 other_unit_files=("src/UTerminalInterface.pas src/UGameMechanics_CLASSIC.pas")
 
+main_program_files=("src/MainClassic")
+
 compile_file () { #Compiles the given file and formats the output
 	fpc $1 -FE"./bin/" | tail -n +4 | egrep --color=always 'Warning|Fatal|$' | GREP_COLOR='0;32' egrep --color=always 'compiled|$'
 	echo ""
@@ -27,6 +29,13 @@ for file_ in $other_unit_files
 do
     compile_file $file_
 done
+
+echo "----- Compiling main programs -----"
+for file_ in $main_program_files
+do
+    compile_file $file_
+done
+
 
 echo 'Done'
 #fpc "src/" -FE"../bin/"
---
+++ src/MainClassic.pas
@@ -0,0 +1,39 @@
+program MainClassic.pas;
+
+uses UConstants, UTShape, UTMino, UTTetrimino, UTMovement, UTNextPieces, UTGeneralInterfaceTypes, UTerminalInterface, UTBoard, UGameMechanics_CLASSIC;
+
+var
+	board : Tboard;
+	IO : IO_Interface;
+	i : byte;
+	tmpNextQueue : TNextPieces;
+
+begin
+	Randomize ();
+
+	board := initBoard(getRandomShape ());
+
+	// Init the queue with random pieces
+	for i := 1 to Cnext_queue_length do
+	begin
+		tmpNextQueue := getNextQueue (board);
+		setIthNextPiece (tmpNextQueue, i , getRandomShape());
+		setNextQueue (board, tmpNextQueue);
+	end;
+
+	IO := newInterface (@initializeDisplay,
+										  @clearScreen,
+										  @clearMatrix,
+										  @showMatrix,
+										  @showTetrimino,
+										  @showNextQueue,
+										  @showHoldPiece,
+										  @showBoard,
+										  @showScore,
+										  @showLevel,
+										  @getPlayerInput);
+
+	repeat
+		computeTurn (board, IO);
+	until getLostStatus (board);
+end.
