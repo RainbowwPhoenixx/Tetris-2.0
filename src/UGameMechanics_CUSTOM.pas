abf6c091b771ba0266482ff75fb6f3215d08e305
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-10-31 18:15:10 +0100

    Relocated tetrimino movement code into the version-specific unit.

--- src/UGameMechanics_CUSTOM.pas
+++ src/UGameMechanics_CUSTOM.pas
@@ -16,6 +16,132 @@ interface
 
 implementation
 
+  // The tetrimino movements depend on the version
+  function shiftTetrominoXAxis (t : TTetrimino; direction: String) : TTetrimino;
+  var
+    i : byte;
+    tmpTetrimino : TTetrimino;
+    tmpMino : TMino;
+  begin
+    tmpTetrimino := t;
+    for i := 1 to 4 do
+    begin
+      tmpMino := shiftMinoXAxis (getIthMino(t, i), direction);
+      setIthMino(tmpTetrimino, i, tmpMino);
+    end;
+
+    shiftTetrominoXAxis := tmpTetrimino;
+  end;
+
+  function shiftTetrominoYAxis (t : TTetrimino; direction: String) : TTetrimino;
+  var
+    i : byte;
+    tmpTetrimino : TTetrimino;
+    tmpMino : TMino;
+  begin
+    tmpTetrimino := t;
+    for i := 1 to 4 do
+    begin
+      tmpMino := shiftMinoYAxis (getIthMino(t, i), direction);
+      setIthMino(tmpTetrimino, i, tmpMino);
+    end;
+
+    shiftTetrominoYAxis := tmpTetrimino;
+  end;
+
+  function rotateTetrimino (t: TTetrimino; amount: String) : TTetrimino;
+  var
+    centerMino, tmpMino, refMino : TMino;
+    tmpTetrimino : TTetrimino;
+    i : byte;
+  begin
+    centerMino := getCenterMino (t);
+    tmpTetrimino := t;
+
+    case amount of
+    '90' :begin
+        for i := 1 to 4 do
+        begin
+          refMino := getIthMino (t, i);
+          tmpMino := refMino;
+          setMinoX (tmpMino, getMinoX(centerMino) + (getMinoY(centerMino) - getMinoY(refMino)));
+          setMinoY (tmpMino, getMinoY(centerMino) - (getMinoX(centerMino) - getMinoX(refMino)));
+
+          setIthMino (tmpTetrimino, i, tmpMino);
+        end;
+        end;
+
+    '180':begin
+        for i := 1 to 4 do
+        begin
+          refMino := getIthMino (t, i);
+          tmpMino := refMino;
+          setMinoX (tmpMino, 2 * getMinoX(centerMino) - getMinoX(refMino)); // From the 180° rotation matrix
+          setMinoY (tmpMino, 2 * getMinoY(centerMino) - getMinoY(refMino));
+
+          setIthMino (tmpTetrimino, i, tmpMino);
+        end;
+        end;
+
+    '-90':begin
+        for i := 1 to 4 do
+        begin
+          refMino := getIthMino (t, i);
+          tmpMino := refMino;
+          setMinoX (tmpMino, getMinoX(centerMino) - (getMinoY(centerMino) - getMinoY(refMino)));
+          setMinoY (tmpMino, getMinoY(centerMino) + (getMinoX(centerMino) - getMinoX(refMino)));
+
+          setIthMino (tmpTetrimino, i, tmpMino);
+        end;
+
+        end;
+    else;
+    end;
+
+  rotateTetrimino := tmpTetrimino;
+  end;
+
+  function moveTetrimino (t: TTetrimino; movement : TMovement) : TTetrimino;
+  var
+    tmpTetrimino : TTetrimino;
+  begin
+    tmpTetrimino := t;
+    case movement of // L, R, SD, HD, CW, CCW, R180, HOLD
+    LFT: begin
+      tmpTetrimino := shiftTetrominoXAxis (t, '-');
+       end;
+
+    RGHT: begin
+      tmpTetrimino := shiftTetrominoXAxis (t, '+');
+       end;
+
+    SD: begin
+      tmpTetrimino := shiftTetrominoYAxis (t, '-');
+       end;
+
+    HD: begin
+      // Not implemented yet. Hard drop depends on the minos inside of the matrix, it will probably not be implemented here.
+       end;
+
+    CW: begin
+      tmpTetrimino := rotateTetrimino (t, '-90');
+       end;
+
+    CCW: begin
+      tmpTetrimino := rotateTetrimino (t, '90');
+       end;
+
+    R180: begin
+      tmpTetrimino := rotateTetrimino (t, '180');
+       end;
+
+    HOLD: begin
+      // Basically same as HD
+       end;
+    NOTHING:;
+    end;
+  moveTetrimino := tmpTetrimino;
+  end;
 
   procedure runGame (IO : IO_Interface);
   var
--- src/UTTetrimino.pas
+++ src/UTTetrimino.pas
@@ -18,8 +18,6 @@ interface
 	// Useful
 	function getCenterMino (t: TTetrimino) : TMino;
 	procedure setCenterMino (var t: TTetrimino; mino : Tmino);
-
-	function moveTetrimino (t: TTetrimino; movement : TMovement) : TTetrimino;
 	function areTetriminoIdentical (t1, t2 : TTetrimino) : Boolean;
 
 	// Function to create tetriminos
@@ -48,92 +46,6 @@ implementation
 	procedure setTetriminoShape (var t: TTetrimino; shape : TShapeTetrimino);
 	begin
 		t.shape := shape;
-	end;
-
-	//Private functions
-	function shiftTetrominoXAxis (t : TTetrimino; direction: String) : TTetrimino;
-	var
-		i : byte;
-		tmpTetrimino : TTetrimino;
-		tmpMino : TMino;
-	begin
-		tmpTetrimino := t;
-		for i := 1 to 4 do
-		begin
-			tmpMino := shiftMinoXAxis (getIthMino(t, i), direction);
-			setIthMino(tmpTetrimino, i, tmpMino);
-		end;
-
-		shiftTetrominoXAxis := tmpTetrimino;
-	end;
-
-
-	function shiftTetrominoYAxis (t : TTetrimino; direction: String) : TTetrimino;
-	var
-		i : byte;
-		tmpTetrimino : TTetrimino;
-		tmpMino : TMino;
-	begin
-		tmpTetrimino := t;
-		for i := 1 to 4 do
-		begin
-			tmpMino := shiftMinoYAxis (getIthMino(t, i), direction);
-			setIthMino(tmpTetrimino, i, tmpMino);
-		end;
-
-		shiftTetrominoYAxis := tmpTetrimino;
-	end;
-
-	function rotateTetrimino (t: TTetrimino; amount: String) : TTetrimino;
-	var
-		centerMino, tmpMino, refMino : TMino;
-		tmpTetrimino : TTetrimino;
-		i : byte;
-	begin
-		centerMino := getCenterMino (t);
-		tmpTetrimino := t;
-
-		case amount of
-		'90' :begin
-				for i := 1 to 4 do
-				begin
-					refMino := getIthMino (t, i);
-					tmpMino := refMino;
-					setMinoX (tmpMino, getMinoX(centerMino) + (getMinoY(centerMino) - getMinoY(refMino)));
-					setMinoY (tmpMino, getMinoY(centerMino) - (getMinoX(centerMino) - getMinoX(refMino)));
-
-					setIthMino (tmpTetrimino, i, tmpMino);
-				end;
-			  end;
-
-		'180':begin
-				for i := 1 to 4 do
-				begin
-					refMino := getIthMino (t, i);
-					tmpMino := refMino;
-					setMinoX (tmpMino, 2 * getMinoX(centerMino) - getMinoX(refMino)); // From the 180° rotation matrix
-					setMinoY (tmpMino, 2 * getMinoY(centerMino) - getMinoY(refMino));
-
-					setIthMino (tmpTetrimino, i, tmpMino);
-				end;
-			  end;
-
-		'-90':begin
-				for i := 1 to 4 do
-				begin
-					refMino := getIthMino (t, i);
-					tmpMino := refMino;
-					setMinoX (tmpMino, getMinoX(centerMino) - (getMinoY(centerMino) - getMinoY(refMino)));
-					setMinoY (tmpMino, getMinoY(centerMino) + (getMinoX(centerMino) - getMinoX(refMino)));
-
-					setIthMino (tmpTetrimino, i, tmpMino);
-				end;
-
-			  end;
-		else;
-		end;
-
-	rotateTetrimino := tmpTetrimino;
 	end;
 
 	// Useful
@@ -204,48 +116,6 @@ implementation
 
 		tetri.shape := shape;
 		newTetrimino := tetri;
-	end;
-
-	function moveTetrimino (t: TTetrimino; movement : TMovement) : TTetrimino;
-	var
-		tmpTetrimino : TTetrimino;
-	begin
-		tmpTetrimino := t;
-		case movement of // L, R, SD, HD, CW, CCW, R180, HOLD
-		LFT: begin
-			tmpTetrimino := shiftTetrominoXAxis (t, '-');
-		   end;
-
-		RGHT: begin
-			tmpTetrimino := shiftTetrominoXAxis (t, '+');
-		   end;
-
-		SD: begin
-			tmpTetrimino := shiftTetrominoYAxis (t, '-');
-		   end;
-
-		HD: begin
-			// Not implemented yet. Hard drop depends on the minos inside of the matrix, it will probably not be implemented here.
-		   end;
-
-		CW: begin
-			tmpTetrimino := rotateTetrimino (t, '-90');
-		   end;
-
-		CCW: begin
-			tmpTetrimino := rotateTetrimino (t, '90');
-		   end;
-
-		R180: begin
-			tmpTetrimino := rotateTetrimino (t, '180');
-		   end;
-
-		HOLD: begin
-			// Basically same as HD
-		   end;
-		NOTHING:;
-		end;
-	moveTetrimino := tmpTetrimino;
 	end;
 
 	function areTetriminoIdentical (t1, t2 : TTetrimino) : Boolean;
